4701177471b7823e565735f94be3ff055d390e33
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2024-09-04 16:03:17 +0200

    add connection to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,29 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js'
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.get('/', (request, reponse) => {
-    console.log(request)
-    return reponse.status(234).send('Wellcome to my mern app')
+mongoose
+.connect(mongoDBURL)
+.then(() => {
+    console.log('connected to database')
+    app.get('/', (request, reponse) => {
+        console.log(request)
+        return reponse.status(234).send('Wellcome to my mern app')
+    })    
+}).catch((error) => {
+    console.error(error)
 })
 
 
-app.listen(PORT, () => {
-    console.log(`server listening on port ${PORT}`)
-})
+
+
+
+
+
+
+
+
+
+
