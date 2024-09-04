f47126b9e36b32dba526945e29aa530d0147ab62
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2024-09-04 15:54:02 +0200

    add mongoos, get req

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
-import { PORT } from './config.js'
+import { PORT, mongoDBURL } from './config.js'
 
 const app = express();
+
+app.get('/', (request, reponse) => {
+    console.log(request)
+    return reponse.status(234).send('Wellcome to my mern app')
+})
+
 
 app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`)
