4ee494308d3cc42ff757659f8b6c66e201b0cdc7
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2024-09-04 21:52:48 +0200

    done for td

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,42 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js'
 import mongoose from 'mongoose';
+import { Cereal } from './models/ceralModel.js';
 
 const app = express();
+
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
     console.log(req)
     return res.status(200).send('Wellcome to my mern ap')
 })  
 
-app.post('/', (req, res) => {
+app.post('/cereals', async (req, res) => {
+
+    try {
+        if (
+            !req.body.title || 
+            !req.body.publishYear
+        ) {
+            return res.status(400).send({message: 'please send all required fields'})
+        }
+        const newCereal = {
+            title: req.body.title,
+            publishYear: req.body.publishYear
+        }
+
+        const cereal = await Cereal(newCereal) 
+        
+        return res.status(201).send(cereal)
+
+    } catch (err) {
+        console.error(err)
+        res.status(500).send({message: err.message})        
+    }
+
+
     res.send('got a post req...')
     return res.status(200).send('Wellcome to my mern app')
 })
