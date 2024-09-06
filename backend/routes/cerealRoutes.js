d0ba092a0c7ba501eebe5296efd1802f50849f04
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2024-09-06 12:23:51 +0200

    add def prog to post route

--- backend/routes/cerealRoutes.js
+++ backend/routes/cerealRoutes.js
@@ -3,16 +3,32 @@ import { Cereal } from '../models/ceralModel.js'
 
 const router = express.Router()
 
+
+//all paths are 'predefined with "cereal" i express-router middleware
+
 //get all cereals
 router.get('/', async (req, res) => {
-    console.log('get all from db requested')
-
     const data = await Cereal.find({})
 
     return res.status(200).json(data)
 })
 
 //get cereal by id
+/* 
+  UNDER CONSTRUCTION
+router.get('/:id', async (req, res) => {
+   try {
+    const { id } = req.params
+    const cereal = await Cereal.findById(id)
+
+    return res.status(200).json(cereal)    
+   } catch (error) {
+    res.status(400).send({message: `Cereal with id of ${id} not found`})
+   }
+
+    
+}) */
+
 router.get('/:id', async (req, res) => {
     console.log('get by id from db requested')
 
@@ -25,14 +41,18 @@ router.get('/:id', async (req, res) => {
 
 //post one cereal
 router.post('/', async (req, res) => {
-
-    const { title } = req.body
     try {
+        const { title } = req.body
+        if (!title) {
+        return res.status(400).send({message: 'please provide all requested field'})
+        }
+        //create cereal obj
         const cereal = await Cereal.create({ title })
         res.status(200).json(cereal)
     } catch (error) {
         res.status(400).json({error: error.message})
-    }})
+    }
+})
 
 //update new cereal
 router.put('/:id', async (req,res) => {
