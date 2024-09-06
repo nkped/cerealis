0710514deeb88e0a1f534af79ec35ebe659fa8af
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2024-09-06 11:23:22 +0200

    crud refactored with express router

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js'
 import mongoose from 'mongoose';
-import { Cereal } from './models/ceralModel.js';
+import cerealRoutes from './routes/cerealRoutes.js';
 
 const app = express();
 
@@ -13,51 +13,7 @@ app.get('/', (req, res) => {
     return res.status(200).send('Wellcome to my mern ap')
 })
 
-//get all cereal
-app.get('/cereals', async (req, res) => {
-    console.log('get all from db requested')
-
-    const data = await Cereal.find({})
-
-    return res.status(200).json(data)
-})
-
-//get cereal by id
-app.get('/cereals/:id', async (req, res) => {
-    console.log('get by id from db requested')
-
-    const { id } = req.params
-
-    const cereal = await Cereal.findById(id)
-
-    return res.status(200).json(cereal)
-})
-
-app.post('/cereals', async (req, res) => {
-
-    const { title } = req.body
-    try {
-        const cereal = await Cereal.create({ title })
-        res.status(200).json(cereal)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }})
-
-app.put('/cereals/:id', async (req,res) => {
-    const { id } = req.params
-    const result = await Cereal.findByIdAndUpdate(id, req.body)
-
-    return res.status(200).send({message: 'was updated'})
-
-})
-
-//delete one cereal
-app.delete('/cereals/:id', async (req,res) => {
-    const { id } = req.params
-    const cereal = await Cereal.findByIdAndDelete(id)    
-
-    return res.status(200).send({message: 'cereal was deleted'})
-})
+app.use('/cereals', cerealRoutes)
 
 
 mongoose
---
+++ backend/routes/cerealRoutes.js
@@ -0,0 +1,55 @@
+import express from 'express'
+import { Cereal } from '../models/ceralModel.js'
+
+const router = express.Router()
+
+
+//get all cereal
+router.get('/', async (req, res) => {
+    console.log('get all from db requested')
+
+    const data = await Cereal.find({})
+
+    return res.status(200).json(data)
+})
+
+//get cereal by id
+router.get('/:id', async (req, res) => {
+    console.log('get by id from db requested')
+
+    const { id } = req.params
+
+    const cereal = await Cereal.findById(id)
+
+    return res.status(200).json(cereal)
+})
+
+router.post('/', async (req, res) => {
+
+    const { title } = req.body
+    try {
+        const cereal = await Cereal.create({ title })
+        res.status(200).json(cereal)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }})
+
+router.put('/:id', async (req,res) => {
+    const { id } = req.params
+    const result = await Cereal.findByIdAndUpdate(id, req.body)
+
+    return res.status(200).send({message: 'was updated'})
+
+})
+
+//delete one cereal
+router.delete('/:id', async (req,res) => {
+    const { id } = req.params
+    const cereal = await Cereal.findByIdAndDelete(id)    
+
+    return res.status(200).send({message: 'cereal was deleted'})
+})
+
+
+
+export default router
